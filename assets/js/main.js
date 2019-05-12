7d89efba64c0f9224a027e05967a191a45183db1
TheArtisto <drogon.team@gmail.com>
2019-05-12 04:10:41 +0300

    js DZ

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+var name = 'VICTORIA', age = 27, x = 'true', tasks = [2, 3, 4], vasya = { name: 'Вася' }, i = 5, n = 20, k, z = 89.279;
+var adres = 'Partizanskaya street 24 township svetloe'
+console.log(name);
+console.log(age);
+console.log(x);
+console.log(tasks);
+console.log(vasya);
+console.log(age.toFixed(2));
+console.log(++i);
+console.log(i++);
+console.log(i);
+
+n += 20;
+console.log(n);
+n -= 5;
+console.log(n);
+n *= 5;
+console.log(n);
+n += 5;
+console.log(n);
+n /= 2;
+console.log(n);
+n %= 7;
+console.log(n);
+
+console.log(Math.PI);
+
+console.log(Math.round(z));
+
+console.log(Math.random()*200);
+console.log(Math.pow(2,5));
+
+console.log(name.length);
+
+console.log(adres.indexOf("zan"));
+console.log(adres.indexOf("township"));
+
+console.log(adres);
+console.log(adres.replace('street','road').replace('township','settlement'));
+console.log(adres.toUpperCase());
+console.log(name.toLowerCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
