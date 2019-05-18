330d8cbc94d3e9fb92aadb691420f0a649cf2189
TheArtisto <drogon.team@gmail.com>
2019-05-18 03:53:24 +0300

    'DZ_JS_16'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,61 @@
-var name = 'VICTORIA', age = 27, x = 'true', tasks = [2, 3, 4], vasya = { name: 'Васян' }, i = 5, n = 20, k, z = 89.279;
-var adres = 'Partizanskaya street 24 township svetloe'
-console.log(name);
-console.log(age);
-console.log(x);
-console.log(tasks);
-console.log(vasya);
-console.log(age.toFixed(2));
-console.log(++i);
-console.log(i++);
-console.log(i);
+var name = 'Alexander';
+  if (name === '') {
+    console.log('Нет имени')
+  } else console.log(name);
 
-n += 20;
-console.log(n);
-n -= 5;
-console.log(n);
-n *= 5;
-console.log(n);
-n += 5;
-console.log(n);
-n /= 2;
-console.log(n);
-n %= 7;
-console.log(n);
-
-console.log(Math.PI);
-
-console.log(Math.round(z));
-
-console.log(Math.random()*200);
-console.log(Math.pow(2,5));
-
-console.log(name.length);
-
-console.log(adres.indexOf("zan"));
-console.log(adres.indexOf("township"));
-
-console.log(adres);
-console.log(adres.replace('street','road').replace('township','settlement'));
-console.log(adres.toUpperCase());
-console.log(name.toLowerCase());
+  
 
 
+var browser = {
+  moz: 'mozila',
+  saf: 'safari',
+  op: 'opera',
+  tor: 'tor',
+  ch: function () {
+    return 'chrome';
+  }
+};
+console.log(browser);
+
+for (i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+};
+
+var postNumb = 8899,
+  status;
+switch (postNumb) {
+  case 5566: status = 'Dnepr'; break
+  case 7788: status = 'Lugansk'; break
+  case 8899: status = 'Poltava'; break
+  case 4455: status = 'Kremenchug'; break
+  case 3344: status = 'Sumi'; break
+  default: status = 'Неизвестный код';
+}
+console.log(postNumb + ' - ' + status);
+
+var age = 51;
+access = age <= 18 ? 'Доступ закрыт' : age <= 50 ? 'Доступ ограничен' : 'Доступ открыт';
+console.log(access);
+
+var seven = 0;
+do {
+  console.log(seven);
+  seven++;
+} while (seven < 8);
 
 
+function dot(arg1, arg2, callback) {
+  console.log('Число arg1 в степени arg2');
+  var bg = Math.pow(arg1, arg2);
+  callback(bg);
+}
 
-
-
-
-
-
-
+dot(2, 10, function (power) {
+  console.log('Результат arg3' + ' = ' + power);
+  
+});
 
 
 
